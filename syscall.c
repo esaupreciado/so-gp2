54c0b0175c98b9f15e4e5ed2cdb42f6f354d534e
Esau <esauplm@gmail.com>
2019-04-03 02:03:54 +0000

    Parte 1 de proyecto

--- syscall.c
+++ syscall.c
@@ -105,6 +105,8 @@ extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_shutdown(void);
 extern int sys_reboot(void);
+extern int sys_setpriority(void);
+extern int sys_getpriority(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -130,7 +132,43 @@ static int (*syscalls[])(void) = {
 [SYS_close]   sys_close,
 [SYS_shutdown] sys_shutdown,
 [SYS_reboot]  sys_reboot,
+[SYS_setpriority] sys_setpriority,
+[SYS_getpriority] sys_getpriority,
 };
+
+
+void
+printsyscall(int num)
+{
+	const char* syscalls[25];
+	syscalls[0] = "fork";
+	syscalls[1] = "exit";
+	syscalls[2] = "wait";
+	syscalls[3] = "pipe";
+	syscalls[4] = "read";
+	syscalls[5] = "kill";
+	syscalls[6] = "exec";
+	syscalls[7] = "fstat";
+	syscalls[8] = "chdir";
+	syscalls[9] = "dup";
+	syscalls[10] = "getpid";
+	syscalls[11] = "sbrk";
+	syscalls[12] = "sleep";
+	syscalls[13] = "uptime";
+	syscalls[14] = "open";
+	syscalls[15] = "write";
+	syscalls[16] = "mknod";
+	syscalls[17] = "unlink";
+	syscalls[18] = "link";
+	syscalls[19] = "mkdir";
+	syscalls[20] = "close";
+	syscalls[21] = "shutdown";
+	syscalls[22] = "reboot";
+	syscalls[23] = "setpriority";
+	syscalls[24] = "getpriority";
+	
+	cprintf("%s -> %d\n", syscalls[num-1], num);
+}
 
 void
 syscall(void)
@@ -140,6 +178,7 @@ syscall(void)
 
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+	  printsyscall(num);
     curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
